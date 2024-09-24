a476e8b0fbf8cd813cead781fca17e331a984e7a
yunsol17 <yunsol1007@naver.com>
2024-09-24 11:16:50 +0900

    Drill 3  제출 (2023182035 조윤솔)

--- character_grass.py
+++ character_grass.py
@@ -25,13 +25,38 @@ def run_left():
     print('LEFT')
     for y in range(0,600,10):
         draw_boy(0,y)
-
+def run_t_top():
+    print('tTOP')
+    for x in range(0,600,10):
+        draw_boy(x,600)
+def run_t_right():
+    print('tRIGHT')
+    for y in range(600,0,-10):
+        draw_boy(600,y)
+def run_t_bottom():
+    print('tBOTTOM')
+    for x in range(600,0,-10):
+        draw_boy(x,0)
+def run_t_diagonal():
+    print('DIAGONAL')
+    y=0
+    for x in range(600,0,-10):
+        y=y+10
+        draw_boy(x,y)
+    
+    
 def run_rectangle():
     print('RECTANGLE')
     run_top()
     run_right()
     run_bottom()
     run_left()
+    
+def run_triangle():
+    print('TRIANGLE')
+    run_t_top()
+    run_t_right()
+    run_t_diagonal()
     
 
 def run_circle():
@@ -47,8 +72,8 @@ def run_circle():
 
 while True:
     
-    #run_circle()
+    run_circle()
     run_rectangle()
-    break
+    run_triangle()
     
 close_canvas()
