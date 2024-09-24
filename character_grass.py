62773d88993c9a63ad9155aa3dde9e881e5793c4
yunsol17 <yunsol1007@naver.com>
2024-09-24 10:45:42 +0900

    draw_boy함수 추가run_top, run_right 함수 완성

--- character_grass.py
+++ character_grass.py
@@ -4,12 +4,19 @@ open_canvas()
 
 grass = load_image('grass.png')
 boy = load_image('character.png')
-
+def draw_boy(x,y):
+    clear_canvas_now()
+    boy.draw_now(x,y)
+    delay(0.01)
+    
 def run_top():
     print('TOP')
-    pass
+    for x in range(0,800,10):
+        draw_boy(x,550)
 def run_right():
     print('RIGHT')
+    for y in range(600,0,-10):
+        draw_boy(800,y)
     pass
 def run_bottom():
     print('BOTTOM')
@@ -25,26 +32,21 @@ def run_rectangle():
     run_bottom()
     run_left()
     
-    pass
 
 def run_circle():
     print('CIRCLE')
 
     r,cx,cy=300,800//2,600//2
     
-    for d in range(0,360):
+    for d in range(0,360,1):
       x=r*math.cos(math.radians(d))+cx
       y=r*math.sin(math.radians(d))+cy
-        
-    clear_canvas_now()
-    boy.draw_now(x,y)
-    delay(0.01)
+      draw_boy(x,y)
 
-    pass
 
 while True:
     
-   # run_circle()
+    #run_circle()
     run_rectangle()
     break
     
