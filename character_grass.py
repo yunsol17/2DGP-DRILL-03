449574c0774bd7d42debb7d7a2b8fbe2aa36ecc3
yunsol17 <yunsol1007@naver.com>
2024-09-24 09:49:31 +0900

    전제적인 구조 완성

---
+++ character_grass.py
@@ -0,0 +1,21 @@
+from pico2d import *
+import math
+
+def run_rectangle():
+    pass
+
+def run_circle():
+    pass
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+
+while True:
+    run_rectangle()
+    run_circle()
+    
+
+    
+close_canvas()
