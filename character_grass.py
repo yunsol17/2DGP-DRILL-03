c6e1f2ed8fb14a84870207f56b5349c61fc39d5e
yunsol17 <yunsol1007@naver.com>
2024-09-24 10:24:56 +0900

    사각형 함수 뼈대 완성

--- character_grass.py
+++ character_grass.py
@@ -5,8 +5,22 @@ open_canvas()
 grass = load_image('grass.png')
 boy = load_image('character.png')
 
+def run_top():
+    pass
+def run_right():
+    pass
+def run_bottom():
+    pass
+def run_left():
+    pass
+
 def run_rectangle():
     print('RECTANGLE')
+    run_top()
+    run_right()
+    run_bottom()
+    run_left()
+    
     pass
 
 def run_circle():
